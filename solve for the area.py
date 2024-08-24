55cc7b1ebff3116c3722adcc77ef0f3f9620d069
nellis98 <nellis@students.mchenry.edu>
2024-08-24 13:25:12 -0500

    Add files via upload

---
+++ solve for the area.py
@@ -0,0 +1,8 @@
+length = 5
+width = 11
+print("length = 5")
+print("width = 11")
+area = length*width
+print("area = length * width")
+print(area)
+print("\nThe area of the rectangle with a length of 5 and a width of 11 is 55")
