184c39266cd442f510852c091270571954d2c7a3
nellis98 <nellis@students.mchenry.edu>
2024-08-24 12:05:58 -0500

    Add files via upload

---
+++ how to code a variable.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 2
+print("Literal:", x)
+
+# Integer
+y = -7
+print("Integer:", y)
+
+# Float
+z = 3.5
+print("Float:", z)
+
+# String
+name = "Nate"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
