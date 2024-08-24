03f8627485e3e1c4074872af5c470d3eb0be7a98
nellis98 <nellis@students.mchenry.edu>
2024-08-24 11:18:17 -0500

    Add files via upload

---
+++ Knock-Knock Joke Telling Program.py
@@ -0,0 +1,8 @@
+# Knock-Knock Joke Telling Program
+print("Knock, knock.")
+print("Who's there?")
+print("Peas.")
+print("Peas who?")
+print("Peas open the door for me.")
+# Knock-Knock code with escape characters
+print("Knock, knock.\nWho's there?\nPeas.\nPeas who?\nPeas open the door for me.")
