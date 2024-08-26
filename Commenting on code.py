e218d594f57fe741e54f88956cd0492955675a6f
nellis98 <nellis@students.mchenry.edu>
2024-08-26 17:22:59 -0500

    Add files via upload

---
+++ Commenting on code.py
@@ -0,0 +1,5 @@
+# This program determines the miles per gallon of a vehicle based on the size of its gas tank and the distance it is able to travel.
+tank_size = 15 #This is the variable for the number of gallons in a tank.
+distance = 600 #This is the variable for the number of miles that the vehicle is able to drive.
+mpg = distance / tank_size #This will solve for the miles per gallon by dividing the distance in miles by the tank size in gallons.
+print("Miles per gallon:", mpg) #This will print the number of miles per gallon based on the values assigned to tank size and distance.
