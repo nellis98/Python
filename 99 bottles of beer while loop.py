c411b68d2690905e09b276c2582d860da79699c9
nellis98 <nellis@students.mchenry.edu>
2024-09-05 16:02:08 -0500

    Add files via upload

---
+++ 99 bottles of beer while loop.py
@@ -0,0 +1,6 @@
+n = 99
+while n > 1:
+    print(f"{n} bottles of beer on the wall \n {n} bottles of beer \n Take one down, pass it around \n {n-1} bottles of beer on the wall!")
+    n -= 1
+else:
+    print(f"1 bottle of beer on the wall \n 1 bottle of beer \n Take one down, pass it around \n No bottles of beer on the wall!")
