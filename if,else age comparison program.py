4c3f15b9b6e4cde07b8e065b5a20658fb225b791
nellis98 <nellis@students.mchenry.edu>
2024-09-05 13:42:33 -0500

    Add files via upload

---
+++ if,else age comparison program.py
@@ -0,0 +1,18 @@
+age = int(input("Age:"))
+print(f"Age:{age}")
+if age >= 16:
+    print("Of legal age to drive")
+else:
+    print("Too young to drive")
+if age >= 18:
+    print("Of legal age to vote")
+else:
+    print("Too young to vote")
+if age >= 21:
+    print("Of legal age to buy alcohol")
+else:
+    print("Too young to buy alcohol")
+if age >= 65:
+    print("Eligible for a senior discount")
+else:
+    print("Too young for senior discount")
