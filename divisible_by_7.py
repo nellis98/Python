92abbe4043e70f1de99160bff0d94f8d82a2940c
nellis98 <nellis@students.mchenry.edu>
2024-09-05 16:14:08 -0500

    Add files via upload

---
+++ divisible_by_7.py
@@ -0,0 +1,9 @@
+#creates a loop lasting 300 interations of i
+for i in range(1,301):
+    #identifies numbers divisible by 7
+    if i%7 == 0:
+        #prints numbefs divisible by 7
+        print(i)
+    #continues if number is not divisible by 7
+    else:
+        continue
