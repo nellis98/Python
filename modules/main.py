096ef01c56b9650e5f77519f1cbf6865c45e9484
nellis98 <nellis@students.mchenry.edu>
2024-10-01 12:51:21 -0500

    Add files via upload

---
+++ modules/main.py
@@ -0,0 +1,16 @@
+from math_operations import calculator, geometry
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
+
+result = geometry.area_of_a_rectangle(3, 4)
+print("Area of a rectangle result: ", result)
+
+result = geometry.area_of_a_triangle(4, 5)
+print("Area of a tringle result: ", result)
+
+result = geometry.area_of_a_circle(2)
+print("Area of a circle result: ", result)
---
+++ modules/math_operations/geometry.py
@@ -0,0 +1,9 @@
+import math
+def area_of_a_rectangle(a, b):
+    return a * b 
+
+def area_of_a_triangle(a, b):
+    return .5 * a * b
+
+def area_of_a_circle(a):
+    return math.pi * a ** 2
